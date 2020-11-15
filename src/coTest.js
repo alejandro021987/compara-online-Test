e5b15ca0409f03596a0d955f8f9af7a439488874
Alejandro Avalo <avavalo@falabella.com.ar>
2020-11-15 17:11:01 -0300

    improvements in function updatePrice

--- src/coTest.js
+++ src/coTest.js
@@ -16,15 +16,15 @@ class CarInsurance {
   updatePrice() {
     this.products.forEach(product => {
       switch (product.name) {
-        case 'Low Coverage', 'Medium Coverage', 'Super Sale':
+        case 'Low Coverage': case 'Medium Coverage': case 'Super Sale':  case 'Full Coverage':
           if ( product.sellIn > 0)
             product.price += conf.coverages[product.name].upgradeVal;
           else {
             product.price += 2*conf.coverages[product.name].upgradeVal;
           }
           break;
-        case 'Full Coverage', 'Special Full Coverage':
-          if(product.sellIn <= 0){
+        case 'Special Full Coverage':
+          if(product.sellIn <= 0 ){
             product.price = 0;
           } else if (product.sellIn > 10 )
             product.price += conf.coverages[product.name].upgradeVal;
@@ -41,70 +41,13 @@ class CarInsurance {
       if (product.name != 'Mega Coverage') {
         product.sellIn = product.sellIn - 1;
         if ( product.price > conf.MAX_PRICE)
-        product.price = conf.MAX_PRICE;
+          product.price = conf.MAX_PRICE;
       }
+
+      if ( product.price < 0)
+        product.price = 0;
 
     });
-
-/*
-    for (var i = 0; i < this.products.length; i++) {
-      if (this.products[i].name != cov.FULL_COV.name && this.products[i].name != cov.SPECIAL_FUL_COV.name) {
-        if (this.products[i].price > 0) {
-          if (this.products[i].name != cov.MEGA_COV.name) {
-            if(this.products[i].name == cov.SUPER_SALE.name){
-              this.products[i].price = this.products[i].price - cov.SUPER_SALE.degradeVal;
-            }else{
-              this.products[i].price = this.products[i].price - 1;
-            }
-
-          }
-        }
-      } else {
-        if (this.products[i].price < 50) {
-          this.products[i].price = this.products[i].price + 1;
-          if (this.products[i].name == cov.SPECIAL_FUL_COV.name) {
-            if (this.products[i].sellIn < 11) {
-              if (this.products[i].price < 50) {
-                this.products[i].price = this.products[i].price + 1;
-              }
-            }
-            if (this.products[i].sellIn < 6) {
-              if (this.products[i].price < 50) {
-                this.products[i].price = this.products[i].price + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.products[i].name != cov.MEGA_COV.name) {
-        this.products[i].sellIn = this.products[i].sellIn - 1;
-      }
-      if (this.products[i].sellIn < 0) {
-        if (this.products[i].name != cov.FULL_COV.name) {
-          if (this.products[i].name != cov.SPECIAL_FUL_COV.name) {
-            if (this.products[i].price > 0) {
-              if (this.products[i].name != cov.MEGA_COV.name) {
-                if(this.products[i].name == cov.SUPER_SALE.name){
-                  this.products[i].price = this.products[i].price - cov.SUPER_SALE.degradeOff;
-                }else{
-                  this.products[i].price = this.products[i].price - 2;
-                }
-
-              }
-            }
-          } else {
-            this.products[i].price = this.products[i].price - this.products[i].price;
-          }
-        } else {
-          if (this.products[i].price < 50) {
-            this.products[i].price = this.products[i].price + 1;
-          }
-        }
-      }
-    }
-*/
-    
-    
 
     return this.products;
   }
--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,11 @@
 module.exports = {
   coverages: {
-    'Low Coverage' : { upgradeVal: -1, degradeOff:2, code: 1},
-    'Medium Coverage': { upgradeVal: -1, degradeOff:2, code: 2},
-    'Full Coverage': { upgradeVal: 1, degradeOff:2, code: 3},
-    'Special Full Coverage': { upgradeVal: 1, degradeOff:-1, code: 4},
-    'Mega Coverage': { upgradeVal: 0, degradeOff:0, code: 5},
-    'Super Sale': { upgradeVal: -2, degradeOff:4, code: 6}
+    'Low Coverage' : { upgradeVal: -1, code: 1},
+    'Medium Coverage': { upgradeVal: -1, code: 2},
+    'Full Coverage': { upgradeVal: 1, code: 3},
+    'Special Full Coverage': { upgradeVal: 1, code: 4},
+    'Mega Coverage': { upgradeVal: 0, code: 5},
+    'Super Sale': { upgradeVal: -2, code: 6}
   },
   MAX_PRICE: 50
 }
